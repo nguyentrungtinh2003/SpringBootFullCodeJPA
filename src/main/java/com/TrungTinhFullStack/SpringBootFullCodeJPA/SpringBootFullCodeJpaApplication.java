42bda715ddc5dce4af86fc5115455e2734d9a42d
nguyentrungtinh <trungtinhn300@gmail.com>
2024-08-08 10:16:31 +0700

    Create project to study JPA

---
+++ src/main/java/com/TrungTinhFullStack/SpringBootFullCodeJPA/SpringBootFullCodeJpaApplication.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.SpringBootFullCodeJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullCodeJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullCodeJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TrungTinhFullStack/SpringBootFullCodeJPA/SpringBootFullCodeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.SpringBootFullCodeJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullCodeJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

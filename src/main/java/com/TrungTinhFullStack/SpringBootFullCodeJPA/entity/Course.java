27a949736350c47f37b433b684323f9a38c1e02e
nguyentrungtinh <trungtinhn300@gmail.com>
2024-08-10 14:06:56 +0700

    Update Project Full Code Spring Boot JPA

--- src/main/java/com/TrungTinhFullStack/SpringBootFullCodeJPA/entity/Course.java
+++ src/main/java/com/TrungTinhFullStack/SpringBootFullCodeJPA/entity/Course.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -39,4 +41,21 @@ public class Course {
             referencedColumnName = "teacherId"
     )
     private Teacher teacher;
+
+    @ManyToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinTable(
+            name = "student_course_map",
+            joinColumns = @JoinColumn(
+                   name = "course_id",
+                    referencedColumnName = "courseId"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "student_id",
+                    referencedColumnName = "studentId"
+            )
+    )
+    private List<Student> students;
+
 }
